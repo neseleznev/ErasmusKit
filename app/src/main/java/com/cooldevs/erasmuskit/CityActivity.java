1c3bfcce271ba047e8c9096dc7d2de2894dbd9ad
Mario <mprietopereira@gmail.com>
2017-04-29 19:24:51 +0200

    Different theme and other small details

--- app/src/main/java/com/cooldevs/erasmuskit/CityActivity.java
+++ app/src/main/java/com/cooldevs/erasmuskit/CityActivity.java
@@ -3,9 +3,6 @@ package com.cooldevs.erasmuskit;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -20,8 +17,6 @@ import com.squareup.picasso.Picasso;
 import java.util.ArrayList;
 
 public class CityActivity extends AppCompatActivity {
-
-    private boolean isFavorite = false;
 
     private static final String TAG = "CityActivity";
 
@@ -81,21 +76,6 @@ public class CityActivity extends AppCompatActivity {
         });
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new GridLayoutManager(CityActivity.this, 2));
-
-
-        // Floating Action Button
-        final FloatingActionButton button = (FloatingActionButton) findViewById(R.id.star_city_fab);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int drawableIcon = isFavorite ? R.drawable.ic_star_border_black_24dp : R.drawable.ic_star_black_24dp;
-                int message = isFavorite ? R.string.fav_city_remove_snack : R.string.fav_city_add_snack;
-
-                button.setImageDrawable(ContextCompat.getDrawable(CityActivity.this, drawableIcon));
-                Snackbar.make(view, message, Snackbar.LENGTH_SHORT).show();
-                isFavorite = !isFavorite;
-            }
-        });
     }
 
     @Override
